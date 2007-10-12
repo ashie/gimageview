f2cac1aed0ec233f6025d371b98f5b0b9ca9d171
makeinu <makeinu@aeae0eed-423c-0410-9251-95fb8af3d19f>
2007-10-12 15:55:06 +0000

    Fix some warnings.
    git-svn-id: https://gtkmmviewer.svn.sourceforge.net/svnroot/gtkmmviewer/gimageview/trunk@1117 aeae0eed-423c-0410-9251-95fb8af3d19f

--- autogen.sh
+++ autogen.sh
@@ -3,18 +3,6 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-m4dir="m4"
-
-#if test -d $srcdir/$m4dir; then
-#    rm -f $srcdir/acinclude.m4
-#    for m4f in $srcdir/$m4dir/*.m4; do
-#       cat $m4f >> acinclude.m4
-#    done
-#else
-#    echo "Directory '$m4dir' is missing."
-#    exit 1
-#fi
-
 libtoolize --copy --force \
   && aclocal -I m4 \
   && autoheader \
